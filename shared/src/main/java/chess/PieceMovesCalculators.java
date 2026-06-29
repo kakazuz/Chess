e0a65f6c7236ad0386f0647addea933241a86953
kakazuz <zzenichi@byu.edu>
2026-06-28 23:50:40 -0600

    intergrated the algorithm for the bishop moves. LETS GOO

--- shared/src/main/java/chess/PieceMovesCalculators.java
+++ shared/src/main/java/chess/PieceMovesCalculators.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,10 +14,33 @@ public class PieceMovesCalculators {
         @Override
         public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
             ChessPiece piece = board.getPiece(myPosition);
-            if (piece.getPieceType() == ChessPiece.PieceType.BISHOP) {
-                return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+            ChessGame.TeamColor peiceType = piece.getTeamColor();
+            List<ChessMove> moveList = new ArrayList<>();
+
+            int[][] moves = { {1,1}, {1,-1}, {-1,-1}, {-1,1} };
+
+            for (int i = 0; i < 4; i++) {
+                for(int mul = 1; mul < 8; mul++) {
+                    int newRow = myPosition.getRow() + moves[i][0] * mul;
+                    int newCol = myPosition.getColumn() + moves[i][1] * mul;
+
+                    if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8 ) {
+                        break;
+                    }
+                    ChessPiece newPiece = board.getPiece(new ChessPosition(newRow,newCol));
+                    if (newPiece == null ) {
+                        moveList.add(new ChessMove(myPosition, new ChessPosition(newRow,newCol), null));
+                    }
+                    else {
+                        if (piece.getTeamColor() != newPiece.getTeamColor()) {
+                            moveList.add(new ChessMove(myPosition, new ChessPosition(newRow,newCol), null));
+                        }
+                        break;
+                    }
+
+                }
             }
-            return List.of();
+            return moveList;
         }
 
     }
