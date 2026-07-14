8e5864d434068efb5f06ec61bcc743cefa8b84ae
kakazuz <zzenichi@byu.edu>
2026-07-13 21:20:06 -0600

    Finished all the three DAO"S and started to work on the Memory versions

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,11 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void createAuth(AuthData authData) throws DataAccessException;
+    AuthData geAuth(String authToken) throws  DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void clear() throws DataAccessException;
+
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -5,7 +5,6 @@ import model.UserData;
 
 public interface UserDAO {
     void clear() throws DataAccessException;
-
     void createUser(UserData user) throws DataAccessException;
     UserData getUser(String username) throws DataAccessException;
 
