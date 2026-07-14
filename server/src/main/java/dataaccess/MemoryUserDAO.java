e52eab071b98b944c77aff9d626c938625ba5066
kakazuz <zzenichi@byu.edu>
2026-07-13 21:33:30 -0600

    implemented the MemoryUserDAO

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,4 +1,33 @@
 package dataaccess;
 
-public class MemoryUserDAO {
+import model.UserData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryUserDAO implements UserDAO {
+    private final Map<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+        users.clear();
+
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        if(users.containsKey(user)) {
+            throw new DataAccessException("Already taken username");
+        }
+        users.put(user.username(), user);
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        UserData user = users.get(username);
+        if(user == null) {
+            throw new DataAccessException("User not found");
+        }
+        return user;
+    }
 }
