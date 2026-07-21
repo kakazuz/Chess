179b9d66c83a6f8a749a62a84f3cd6a33ccb30b3
kakazuz <zzenichi@byu.edu>
2026-07-20 19:59:57 -0600

    Moved the Test, added dependencies, and made the sqluserdao class not implemented

---
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -0,0 +1,21 @@
+package dataaccess;
+import model.UserData;
+
+public class SQLUserDAO implements UserDAO {
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+        // implement
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        // implement
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        // implement
+    }
+}
