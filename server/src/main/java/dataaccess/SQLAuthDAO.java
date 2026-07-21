0018220fca104bd9fd02c6c7b2e3cccc75ed604f
kakazuz <zzenichi@byu.edu>
2026-07-20 20:23:26 -0600

    Copied the same implementation for the table/initialization from user to auth

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -1,7 +1,37 @@
 package dataaccess;
 import model.AuthData;
 
+import java.sql.SQLException;
+
 public class SQLAuthDAO implements AuthDAO {
+
+    public SQLAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+        CREATE TABLE IF NOT EXISTS auth (
+            authToken VARCHAR(255) NOT NULL,
+            username VARCHAR(255) NOT NULL,
+            PRIMARY KEY (authToken),
+            FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
+        )
+        """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
