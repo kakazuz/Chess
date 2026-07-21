f63cead6e740a45f70d0617df08c06f9f1d04c95
kakazuz <zzenichi@byu.edu>
2026-07-20 20:26:55 -0600

    Copied the same implementation for the table/initialization from user to game

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -2,7 +2,41 @@ package dataaccess;
 import model.GameData;
 import java.util.Collection;
 
+import java.sql.SQLException;
+
 public class SQLGameDAO implements GameDAO {
+
+    public SQLGameDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    private final String[] createStatements = {
+            """
+        CREATE TABLE IF NOT EXISTS game (
+            gameID INT NOT NULL AUTO_INCREMENT,
+            whiteUsername VARCHAR(255) DEFAULT NULL,
+            blackUsername VARCHAR(255) DEFAULT NULL,
+            gameName VARCHAR(255) NOT NULL,
+            game TEXT NOT NULL,
+            PRIMARY KEY (gameID),
+            FOREIGN KEY (whiteUsername) REFERENCES user(username) ON DELETE SET NULL,
+            FOREIGN KEY (blackUsername) REFERENCES user(username) ON DELETE SET NULL
+        )
+        """
+    };
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
