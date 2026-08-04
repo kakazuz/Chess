0a78e496bf953e3a84cd91adb1be660545512c79
kakazuz <zzenichi@byu.edu>
2026-08-03 19:09:24 -0600

    set up a basic command message class

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,5 +1,6 @@
 package websocket.commands;
 
+import chess.ChessMove;
 import java.util.Objects;
 
 /**
@@ -16,10 +17,17 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
+    private ChessMove move;
+
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+    }
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
+        this(commandType, authToken, gameID);
+        this.move = move;
     }
 
     public enum CommandType {
@@ -39,6 +47,10 @@ public class UserGameCommand {
 
     public Integer getGameID() {
         return gameID;
+    }
+
+    public ChessMove getMove() {
+        return move;
     }
 
     @Override
